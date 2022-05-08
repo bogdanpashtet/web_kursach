58987697d6e1abf019a4529e2b8d724027780c51
bogdanpashtet <ruslan.nnov@mail.ru>
2022-05-09 00:15:48 +0300

    first commit

--- index.php
+++ index.php
@@ -7,10 +7,38 @@ $style_link = "
 
 $content = "
     <h1>Категории</h1>
-    <button class='btn cat_button'>Программирование</button>
-    <button class='btn cat_button'>Математика</button>
-    <button class='btn cat_button'>Физика</button>
-    <button class='btn cat_button'>Химия</button>
+    <div class='button_placement'>
+        <button class='btn cat_button'>Программирование</button>
+        <button class='btn cat_button'>Математика</button>
+        <button class='btn cat_button'>Физика</button>
+        <button class='btn cat_button'>Химия</button>
+    </div>
+    <div class='button_placement'>
+        <button class='btn cat_button'>Медицина</button>
+        <button class='btn cat_button'>Литература</button>
+        <button class='btn cat_button'>Искусство</button>
+        <button class='btn cat_button'>Экономика</button>
+    </div>
+    
+    <h1>Последние статьи</h1>
+    
+";
+
+$content = $content . "
+    <h1>Говно</h1>
+    <h1>Говно</h1>
+    <h1>Говно</h1>
+    <h1>Говно</h1>
+    <h1>Говно</h1>
+    <h1>Говно</h1>
+    <h1>Говно</h1>
+    <h1>Говно</h1>
+    <h1>Говно</h1>
+    <h1>Говно</h1>
+    <h1>Говно</h1>
+    <h1>Говно</h1>
+    <h1>Говно</h1>
+    <h1>Говно</h1>
 ";
 
 include_once "layout.html";
