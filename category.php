d1d4d9f88846cf6568f2f3b9e0ee5dea084e5cd4
bogdanpashtet <ruslan.nnov@mail.ru>
2022-05-09 01:00:53 +0300

    first commit

--- category.php
+++ category.php
@@ -1,1 +1,9 @@
 <?php
+
+$title = "Название категории";
+
+$content = "<p>Пизда</p>";
+
+include_once "layout.html";
+
+?>
--- index.php
+++ index.php
@@ -1,28 +1,10 @@
 <?php
+
+session_start();
+
 $title = "Архив статей";
 
-$style_link = "
-    <link rel='stylesheet' href='styles/main_page_style.css'>
-";
-
-$content = "
-    <h1>Категории</h1>
-    <div class='button_placement'>
-        <button class='btn cat_button'>Программирование</button>
-        <button class='btn cat_button'>Математика</button>
-        <button class='btn cat_button'>Физика</button>
-        <button class='btn cat_button'>Химия</button>
-    </div>
-    <div class='button_placement'>
-        <button class='btn cat_button'>Медицина</button>
-        <button class='btn cat_button'>Литература</button>
-        <button class='btn cat_button'>Искусство</button>
-        <button class='btn cat_button'>Экономика</button>
-    </div>
-    
-    <h1>Последние статьи</h1>
-    
-";
+$content = file_get_contents("templates/main_page_template.html");
 
 $content = $content . "
     <h1>Говно</h1>
@@ -41,6 +23,6 @@ $content = $content . "
     <h1>Говно</h1>
 ";
 
-include_once "layout.html";
+require "layout.html";
 
 ?>
