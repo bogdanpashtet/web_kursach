51101fc7f84c929604e8a0001b887abb9044aaa9
bogdanpashtet <ruslan.nnov@mail.ru>
2022-05-10 03:08:37 +0300

    first commit

--- category.php
+++ category.php
@@ -1,31 +1,6 @@
 <?php
 
-switch (($_GET['category'])) {
-    case "programming":
-        $title = "Программирование";
-        break;
-    case "math":
-        $title = "Математика";
-        break;
-    case "physics":
-        $title = "Физика";
-        break;
-    case "chemistry":
-        $title = "Химия";
-        break;
-    case "medicine":
-        $title = "Медицина";
-        break;
-    case "literature":
-        $title = "Литература";
-        break;
-    case "art":
-        $title = "Искусство";
-        break;
-    case "economics":
-        $title = "Экономика";
-        break;
-}
+$title = $_GET['category'];
 
 $link = "";
 $content = '';
