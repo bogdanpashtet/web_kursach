c12d3669654340c7c7565150119e0fc1835f4746
bogdanpashtet <ruslan.nnov@mail.ru>
2022-05-11 00:23:34 +0300

    first commit

--- authorization.php
+++ authorization.php
@@ -1,11 +1,13 @@
 <?php
 $title = "Вход";
+include_once "templates/header.html";
 
 $content = "
   
 ";
+include_once "templates/main_part.html";
 
-include_once "templates/layout.html";
+include_once "templates/footer.html";
 
 
 ?>
