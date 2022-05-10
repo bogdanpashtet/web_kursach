052515601aaabda98755ba91b9cfd1bceb500c5c
bogdanpashtet <ruslan.nnov@mail.ru>
2022-05-10 15:02:54 +0300

    first commit

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $link = "";
 
 require_once("templates/connection.php");
 
-$sql = mysqli_query($link, "SELECT snake_name, name, date FROM articles order by date desc;");
+$sql = mysqli_query($link, "SELECT snake_name, name, date FROM articles order by date desc limit 5;");
 
 $content = file_get_contents("templates/main_page_template.html");
 
@@ -21,7 +21,6 @@ $content = $content . '
 while($row = mysqli_fetch_array($sql)) {
     $content = $content . '<a class="articles" href="article.php?snake_name=' . $row['0'] . '">' . $row['1'] . '<div>' . $row['2'] . '</div>' . '</a>';
 }
-
 $content = $content . '</div>';
 // --------- выводим статьи ---------
 
