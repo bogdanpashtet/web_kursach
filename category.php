4e7df6a6db231500951639c0aea970b803d1a4ce
bogdanpashtet <ruslan.nnov@mail.ru>
2022-05-10 04:27:52 +0300

    first commit

--- category.php
+++ category.php
@@ -7,7 +7,7 @@ $content = '';
 
 require_once("templates/connection.php");
 
-$sql = mysqli_query($link, "SELECT name, snake_name FROM articles where category = '" . $title . "';");
+$sql = mysqli_query($link, "SELECT name, snake_name, date FROM articles where category = '" . $title . "';");
 
 //if ($sql) {
 //    $content = $content . '<p>Успех!.</p>';
@@ -17,14 +17,33 @@ $sql = mysqli_query($link, "SELECT name, snake_name FROM articles where category
 
 $rows_name = [];
 $rows_snake_case = [];
+$rows_snake_date = [];
+
 while($row = mysqli_fetch_array($sql)) {
     $rows_name[] = $row['0'];
     $rows_snake_case[] = $row['1'];
+    $rows_snake_date[] = $row['2'];
 }
 
-$content = '
-<div><a href="article.php?snake_name=' . $rows_snake_case['1'] . '">' . $rows_name['1'] . '</a></div>' . '
-<div><a href="article.php?snake_name=' . $rows_snake_case['0'] . '">' . $rows_name['0'] . '</a></div>'
+$content = $content . '
+<style>
+.articles {
+    display: flex;
+    flex-direction: column;
+    color: #333333;
+    font-size: 14px;
+    margin: 3%;
+    padding: 3%;
+    border: #46a049 3px solid;
+    text-decoration: none !important;
+}
+
+</style>
+<div>
+<a class="articles" href="article.php?snake_name=' . $rows_snake_case['0'] . '">' . $rows_name['0'] . '<div>' . $rows_snake_date['0'] . '</div>' . '</a>' . '
+<a class="articles" href="article.php?snake_name=' . $rows_snake_case['1'] . '">' . $rows_name['1'] . '<div>' . $rows_snake_date['1'] . '</div>' . '</a>' . '
+<a class="articles" href="article.php?snake_name=' . $rows_snake_case['2'] . '">' . $rows_name['2'] . '<div>' . $rows_snake_date['2'] . '</div>' . '</a>' .
+    '</div>'
 ;
 
 include_once "templates/layout.html";
