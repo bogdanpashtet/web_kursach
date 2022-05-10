9c400c4ca7451dd7a358054199a746553776c8fc
bogdanpashtet <ruslan.nnov@mail.ru>
2022-05-10 03:01:12 +0300

    first commit

--- article.php
+++ article.php
@@ -1,1 +1,25 @@
 <?php
+
+$link = "";
+
+require_once("templates/connection.php");
+
+$sql = mysqli_query($link, "SELECT * FROM articles where snake_name = '" . $_GET['snake_name'] . "';");
+
+$row = mysqli_fetch_array($sql);
+
+$title = $row['name'];
+
+$content = "
+<h1>" . $row['name'] . "</h1>
+
+<div>". $row['text'] ."</div>
+
+<div align='center'>
+    Автор статьи: " . $row['author']."<br>
+    Год написания статьи: ". $row['date'] ."
+</div>";
+
+include_once "templates/layout.html";
+
+?>
--- category.php
+++ category.php
@@ -27,16 +27,30 @@ switch (($_GET['category'])) {
         break;
 }
 
-
 $link = "";
+$content = '';
 
 require_once("templates/connection.php");
 
-$sql = mysqli_query($link, "SELECT text FROM articles;");
+$sql = mysqli_query($link, "SELECT name, snake_name FROM articles where category = '" . $title . "';");
 
-$row = mysqli_fetch_array($sql);
+//if ($sql) {
+//    $content = $content . '<p>Успех!.</p>';
+//} else {
+//    $content = $content . '<p>Произошла ошибка: ' . mysqli_error($link) . '</p>';
+//}
 
-$content = $row['text'];
+$rows_name = [];
+$rows_snake_case = [];
+while($row = mysqli_fetch_array($sql)) {
+    $rows_name[] = $row['0'];
+    $rows_snake_case[] = $row['1'];
+}
+
+$content = '
+<div><a href="article.php?snake_name=' . $rows_snake_case['1'] . '">' . $rows_name['1'] . '</a></div>' . '
+<div><a href="article.php?snake_name=' . $rows_snake_case['0'] . '">' . $rows_name['0'] . '</a></div>'
+;
 
 include_once "templates/layout.html";
 
