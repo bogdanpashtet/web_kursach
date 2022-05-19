df6aec9b3775a8b214bef3c34304c0719657df4f
bogdanpashtet <ruslan.nnov@mail.ru>
2022-05-19 23:43:13 +0300

    first commit

--- authorization.php
+++ authorization.php
@@ -24,23 +24,36 @@ if(array_key_exists('submit_button', $_POST)) {
     $password = md5($_POST['password']);
 
     $check_user = mysqli_query($link, "SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
-    if (mysqli_num_rows($check_user) > 0) {
 
-        $user = mysqli_fetch_assoc($check_user);
 
-        $_SESSION['user'] = [
-            "id" => $user['id'],
-            "login" => $user['login'],
-            "role" => $user['role'],
-            "status" => $user['status']
-        ];
 
-        header('Location: profile.php?id=' . $_SESSION['user']['login']);
 
-    } else {
-        $_SESSION['message'] = 'Не верный логин или пароль';
-        header('Location: /authorization.php');
-    }
+
+        if (mysqli_num_rows($check_user) > 0) {
+
+            $user = mysqli_fetch_assoc($check_user);
+
+        if ($user['status'] == 1) {
+            $_SESSION['user'] = [
+                "id" => $user['id'],
+                "login" => $user['login'],
+                "role" => $user['role'],
+                "status" => $user['status']
+            ];
+
+            header('Location: profile.php?id=' . $_SESSION['user']['login']);
+
+        } else {
+            $_SESSION['message'] = 'Данный пользователь заблокирован.';
+            header('Location: /authorization.php');
+        }
+
+        } else {
+            $_SESSION['message'] = 'Не верный логин или пароль';
+            header('Location: /authorization.php');
+        }
+
+
 }
 
 include_once "templates/footer.html";
